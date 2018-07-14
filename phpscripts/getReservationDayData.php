87e6a41a74e0159a8d68d0a0f90a439ffe10afd0
Stephen <uastephen@gmail.com>
2018-07-13 20:34:16 -0400

    my change

--- phpscripts/getReservationDayData.php
+++ phpscripts/getReservationDayData.php
@@ -4,7 +4,8 @@
 	include('Session.php');
 	include('AppConfig.php');
 	$lookupDate = $_GET['lookupDate'];
-	$lookendDate = $lookupDate + 1;
+	$lookendDate = date_add($lookupDate,date_interval_create_from_date_string("1 days"));
+	echo ($lookendDate)
 	$queryType = $_GET['type'];
 	
 	#if(strlen($_SESSION['login_user'])>0){
@@ -20,7 +21,7 @@
 	$sql = "select hour(Item_Start_Time)as starttime,EquipmentType,Duration_Option ,Sum(Equipment_Count) as Equipment_Count from ReservationItems RI
 	join ValidEquipmentTypes VET on RI.Equipment_ID = VET.ID
 	join ValidEquipmentDurations VED on RI.Equipment_Duration_ID = VED.ID
-		where Item_Start_Time > '$lookupDate' and Item_Start_Time < '$lookendDate' and VET.Inventory_check_required = '$queryType' 
+		where Item_Start_Time > '$lookupDate' and Item_Start_Time < $lookendDate and  VET.Inventory_check_required = '$queryType' 
 	group by Item_Start_Time,EquipmentType,Duration_Option 
 	";
   	      $result = mysqli_query($appdb,$sql,MYSQLI_ASSOC);
