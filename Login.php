3bb8ab77ec7bc732316ea01fb31d3af9c29f0ab0
Dylan Schlabach <dylanschlabach@gmail.com>
2018-07-13 21:00:36 -0400

    lets hope this works

--- Login.php
+++ Login.php
@@ -1,77 +1,65 @@
-<?php
-   include("Config.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = $_POST['username'];
-      $mypassword = $_POST['password']; 
-      $sql = "SELECT User_ID FROM Users WHERE User_Name = '$myusername' and User_Password = '$mypassword' and User_Active = 1";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-        
-      $sql = "SELECT User_Role_ID FROM Users WHERE User_Name = '$myusername' and User_Password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $_SESSION['role_id'] = $row['User_Role_ID'];
-
-         $_SESSION['login_user'] = $myusername;
-        //session_register("myusername");
-	  
-         header("location: Welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
+<!DOCTYPE HTML>
 <html>
-   
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
+	<head>
+		<title>Login Page</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="Login.php" class="logo"><strong>RIMS</strong></a>
+								</header>
+
+							<!-- Content -->
+								<section>
+									<header class="main">
+										<h1>RIMS</h1>
+									</header>
+
+									<span class="image main"></span>
+
+									   <body>
 	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-            </div>
-         </div>
-      </div>
-   </body>
+										  <div>
+											 <div>
+												<div><b>Login</b></div>
+													
+												<div>
+												   
+												   <form action = "" method = "post">
+													  <label>Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
+													  <label>Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
+													  <input type = "submit" value = " Submit "/><br />
+												   </form>
+												   
+												   <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+												</div>
+											 </div>
+										  </div>
+								</section>
+						</div>
+					</div>
+			</div>
+		<!-- Footer -->
+			<footer id="footer">
+				<p class="copyright">&copy; The Tricaso Development Team. All rights reserved.</p>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
 </html>
-<!--hello stev-->
--- Welcome.php
+++ Welcome.php
@@ -2,50 +2,101 @@
    include('Session.php');
 ?>
 
-
 <html ng-app="scotchApp">
 
-<head>
-  <!-- SCROLLS -->
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" />
-  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
-  <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
-  <script src="scripts/WelcomePagescript.js"></script>
-</head>
+	<head>
+		<title>IRMS</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		  <!-- SCROLLS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" />
+		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
+		<script src="scripts/WelcomePagescript.js"></script>
+		
+	</head>
+	<body ng-controller="homeScreenController" class="is-preload">
 
-<!-- define angular controller -->
-<body ng-controller="homeScreenController">
+		<!-- Wrapper -->
+			<div id="wrapper">
 
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">NTR CANOE LIVERY- <?php echo  $_SESSION['login_user'], ' ', $_SESSION['role_id'] ?></a>
-      </div>
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
 
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
-        <li><a href="#customersearch"><i class="fa fa-shield"></i>Reservations</a></li>
-        <li><a href="#contact"><i class="fa fa-comment"></i> Contact</a></li>
-	<li><a href="#admin"><i class="fa fa-suitcase"></i> Admin Console</a></li>
-      </ul>
-    </div>
-  </nav>
+							<!-- Header -->
+								<header id="header">
+									<a href="Welcome.php" class="logo"><strong>RIMS</strong></a>
+								</header>
 
-  <div id="main">
-  
-    <!-- angular templating -->
-		<!-- this is where content will be injected -->
-    <div ng-view></div>
-    
-  </div>
-  
-  <footer class="text-center">
-    <p>IRMS Version 0.1.1 </p>  
-    <p>Brought to you buy the Tricaso development team</p>
-  </footer>
-  
-</body>
+							<!-- Content -->
+								<section>
+									<header class="main">
+										<h1>Welcome</h1>
+									</header>
+									  <div id="main">
+										<div ng-view>
+											<!-- this is where content will be injected -->
+										</div>
+									  </div>
+								</section>
+						</div>
+					</div>
 
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2><a href="#">NTR Canoe Livery - </a></h2>
+											<h4> Hello, <?php echo  $_SESSION['login_user'] ?></h4>
+									</header>
+									<ul>
+										<li><a href="#">Home</a></li>
+										<li><a href="#customersearch">Reservations</a></li>
+										<li><a href="#contact"> Contact</a></li>
+										<li>
+											<span class="opener">User Menu</span>
+											<ul>
+												<li><a href="#admin">Create User</a></li>
+												<li><a href="#admin">Modify User</a></li>
+												<li><a href="#admin">Delete User</a></li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Our contact info</h2>
+									<ul class="contact">
+										<li class="fa-envelope-o"><a href="#">NTR@bright.net</a></li>
+										<li class="fa-phone">(330) 874-2002</li>
+										<li class="fa-home">11358 OH-212, <br/>
+										Bolivar, OH 44612</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">&copy; The Tricaso Development Team. All rights reserved.</p>
+								</footer>
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
-
